c6bcf7cc3c5022cfcad2f9bfcb27470d5dd93e72
Nabaraj Bhandari <nabarajbhandari2005@gmail.com>
2026-05-05 09:23:38 +0545

    try: darr ke age jeet hai; main machine

--- config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- ~/.config/nvim/lua/config/options.lua
-vim.opt.wrap = true -- visual wrap off
-vim.opt.linebreak = true -- don't break lines
-vim.opt.tabstop = 4 -- tab = 4 spaces
-vim.opt.shiftwidth = 4 -- indent = 4 spaces
-vim.opt.softtabstop = 4 -- editing behaves like 4 spaces
-vim.opt.expandtab = true -- use spaces instead of actual tab
--- install.sh
+++ install.sh
@@ -7,107 +7,106 @@ die() {
     exit 1
 }
 
-# [ -d /sys/firmware/efi ] || die "Not UEFI."
-#
-# lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
-# echo ""
-# read -rp "EFI partition: " EFI_PART
-# read -rp "Root partition: " ROOT_PART
-# read -rp "Username: " USERNAME
-# read -rsp "User password: " USER_PASS
-# echo
-# read -rsp "Root password: " ROOT_PASS
-# echo
-# read -rp "Hostname: " HOSTNAME
-#
-# info "Formatting..."
-# mkfs.fat -F32 "$EFI_PART"
-# mkfs.ext4 -F "$ROOT_PART"
-#
-# info "Mounting..."
-# mount "$ROOT_PART" /mnt
-# mkdir -p /mnt/boot
-# mount "$EFI_PART" /mnt/boot
-#
-#
-# info "Installing packages..."
-# pacstrap -K /mnt \
-#     base base-devel linux linux-firmware intel-ucode \
-#     networkmanager \
-#     sway swaybg swayidle foot fuzzel mako waybar \
-#     grim slurp wl-clipboard xdg-desktop-portal-wlr xdg-user-dirs \
-#     pipewire wireplumber pipewire-pulse pipewire-alsa alsa-utils pavucontrol \
-#     brightnessctl playerctl \
-#     power-profiles-daemon \
-#     ttf-jetbrains-mono-nerd noto-fonts noto-fonts-emoji ttf-font-awesome \
-#     tmux neovim git fzf jq \
-#     yazi ffmpegthumbnailer imagemagick poppler \
-#     udiskie udisks2 \
-#     zip unzip p7zip unrar \
-#     firefox mpv imv \
-#     docker docker-compose kubectl terraform ansible \
-#     htop rsync curl wget \
-#     man-db man-pages \
-#     mesa vulkan-intel intel-media-driver \
-#     libva-intel-driver libva-utils \
-#     thermald lm_sensors \
-#     fortune-mod gdu s-tui yad \
-#     sudo
-#
-# genfstab -U /mnt >>/mnt/etc/fstab
-#
-# arch-chroot /mnt /bin/bash <<CHROOT
-# set -e
-#
-# echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-# locale-gen
-# echo "LANG=en_US.UTF-8" > /etc/locale.conf
-#
-# ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
-# hwclock --systohc
-#
-# echo "$HOSTNAME" > /etc/hostname
-# cat >> /etc/hosts << EOF
-# 127.0.0.1 localhost
-# ::1 localhost
-# 127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
-# EOF
-#
-# echo "root:$ROOT_PASS" | chpasswd
-# useradd -m -G wheel,video,input,audio,storage,docker -s /bin/bash $USERNAME
-# echo "$USERNAME:$USER_PASS" | chpasswd
-# sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-#
-# bootctl install
-# cat > /boot/loader/loader.conf << EOF
-# default arch.conf
-# timeout 0
-# editor no
-# EOF
-#
-# ROOT_UUID=\$(blkid -s UUID -o value $ROOT_PART)
-# cat > /boot/loader/entries/arch.conf << EOF
-# title Arch Linux
-# linux /vmlinuz-linux
-# initrd /intel-ucode.img
-# initrd /initramfs-linux.img
-# options root=UUID=\$ROOT_UUID rw quiet loglevel=3
-# EOF
-#
-# systemctl enable NetworkManager docker thermald power-profiles-daemon udisks2
-#
-# CHROOT
+[ -d /sys/firmware/efi ] || die "Not UEFI."
+
+lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
+echo ""
+read -rp "EFI partition: " EFI_PART
+read -rp "Root partition: " ROOT_PART
+read -rp "Username: " USERNAME
+read -rsp "User password: " USER_PASS
+echo
+read -rsp "Root password: " ROOT_PASS
+echo
+read -rp "Hostname: " HOSTNAME
+
+info "Formatting..."
+mkfs.fat -F32 "$EFI_PART"
+mkfs.ext4 -F "$ROOT_PART"
+
+info "Mounting..."
+mount "$ROOT_PART" /mnt
+mkdir -p /mnt/boot
+mount "$EFI_PART" /mnt/boot
+
+info "Installing packages..."
+pacstrap -K /mnt \
+    base base-devel linux linux-firmware intel-ucode \
+    networkmanager \
+    sway swaybg swayidle foot fuzzel mako waybar \
+    grim slurp wl-clipboard xdg-desktop-portal-wlr xdg-user-dirs \
+    pipewire wireplumber pipewire-pulse pipewire-alsa alsa-utils pavucontrol \
+    brightnessctl playerctl \
+    power-profiles-daemon \
+    ttf-jetbrains-mono-nerd noto-fonts noto-fonts-emoji ttf-font-awesome \
+    tmux neovim git fzf jq \
+    yazi ffmpegthumbnailer imagemagick poppler \
+    udiskie udisks2 \
+    zip unzip p7zip unrar \
+    firefox mpv imv \
+    docker docker-compose kubectl terraform ansible \
+    htop rsync curl wget \
+    man-db man-pages \
+    mesa vulkan-intel intel-media-driver \
+    libva-intel-driver libva-utils \
+    thermald lm_sensors \
+    fortune-mod gdu s-tui yad \
+    sudo
+
+genfstab -U /mnt >>/mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash <<CHROOT
+set -e
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+hwclock --systohc
+
+echo "$HOSTNAME" > /etc/hostname
+cat >> /etc/hosts << EOF
+127.0.0.1 localhost
+::1 localhost
+127.0.1.1 $HOSTNAME.localdomain $HOSTNAME
+EOF
+
+echo "root:$ROOT_PASS" | chpasswd
+useradd -m -G wheel,video,input,audio,storage,docker -s /bin/bash $USERNAME
+echo "$USERNAME:$USER_PASS" | chpasswd
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+bootctl install
+cat > /boot/loader/loader.conf << EOF
+default arch.conf
+timeout 0
+editor no
+EOF
+
+ROOT_UUID=\$(blkid -s UUID -o value $ROOT_PART)
+cat > /boot/loader/entries/arch.conf << EOF
+title Arch Linux
+linux /vmlinuz-linux
+initrd /intel-ucode.img
+initrd /initramfs-linux.img
+options root=UUID=\$ROOT_UUID rw quiet loglevel=3
+EOF
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+systemctl enable NetworkManager docker thermald power-profiles-daemon udisks2
+
+CHROOT
 
 H="/mnt/home/$USERNAME"
-GIT="/mnt/usr/bin/git"
 
 mkdir -p "$H/.config/sway"
 cp /mnt/etc/sway/config "$H/.config/sway/config"
 
+sed -i '/^bar {/,/^}/d' "$H/.config/sway/config"
+
 cat >>"$H/.config/sway/config" <<'EOF'
-
-
-info "Point 0"
 
 set $mod Mod4
 
@@ -122,16 +121,13 @@ bindsym XF86AudioPlay exec playerctl play-pause
 bindsym XF86AudioNext exec playerctl next
 bindsym XF86AudioPrev exec playerctl previous
 
-exec pipewire &
-exec pipewire-pulse &
-exec wireplumber &
 exec mako &
 exec udiskie &
 exec waybar &
 exec swayidle -w timeout 300 'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"'
 
-output * scale 2
-output * bg ~/wallpaper.jpg fill
+output * scale 1.5
+output * bg ~/.config/wallpaper.jpg fill
 EOF
 
 mkdir -p "$H/.config/waybar"
@@ -140,7 +136,7 @@ cat >"$H/.config/waybar/config" <<'EOF'
 {
   "layer": "top",
   "position": "top",
-  "height": 30,
+  "height": 34,
   "modules-left": ["sway/workspaces", "sway/window"],
   "modules-center": ["custom/player"],
   "modules-right": ["pulseaudio","backlight","network","cpu","memory","temperature","battery","tray","clock"],
@@ -224,15 +220,12 @@ window#waybar {
 }
 EOF
 
-info "Point 1"
-
-info "Point 2"
-
-curl -sL https://raw.githubusercontent.com/nabaraj-bhandari/archsetup/main/wallpaper.jpg -o "$H/wallpaper.jpg"
+curl -sL https://raw.githubusercontent.com/nabaraj-bhandari/archinstall/main/wallpaper.jpg -o "$H/.config/wallpaper.jpg"
 
 mkdir -p "$H/Pictures" "$H/Downloads" "$H/Projects"
 
 arch-chroot /mnt chown -R "$USERNAME:$USERNAME" "/home/$USERNAME"
+arch-chroot /mnt systemctl --user --global enable pipewire pipewire-pulse wireplumber
 
 umount -R /mnt
 info "Done. Reboot."
